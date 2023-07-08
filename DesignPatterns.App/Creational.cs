97f3719e77ef24741f4a93faf48080719e296784
Leandro Gabriel <leandro@bigiogabriel.com>
2023-07-08 18:34:59 +0100

    Remove comment

--- DesignPatterns.App/Creational.cs
+++ DesignPatterns.App/Creational.cs
@@ -4,9 +4,6 @@ namespace DesignPatterns.App;
 
 public static class Creational
 {
-    /// <summary>
-    /// Client using only interfaces
-    /// </summary>
     public static void RunAbstractFactoryExample()
     {
         Console.WriteLine("Abstract Factory pattern tests");
